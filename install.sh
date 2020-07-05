4ecb3bb2a33a9c5c2a9d0e13ca7f3c0133f0ce66
Carlos Fdez. Llamas <hello@sirikon.me>
2020-07-05 15:38:01 +0200

    Refactor install, working on brightness script

--- install.sh
+++ install.sh
@@ -5,31 +5,50 @@ ROOT="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
 cd "$ROOT"
 
 function main {
+    extend-bashrc
+
+    link-folder-if-not-exists "${ROOT}/config/i3" ~/.config/i3
+    link-folder-if-not-exists "${ROOT}/config/i3status" ~/.config/i3status
+    link-folder-if-not-exists "${ROOT}/config/i3blocks" ~/.config/i3blocks
+
+    pacman-install \
+        vim dmenu gnome-terminal i3blocks firefox pamac \
+        pavucontrol terminus-font noto-fonts-emoji ttf-dejavu \
+        polkit-gnome nitrogen openssh code xorg-xbacklight \
+        keepassxc zenity
+
+    prevent-dropbox-self-update
+}
+
+function extend-bashrc {
     if [ "$(grep -c "# Sirikon dotfiles" ~/.bashrc)" == 0 ]; then
         bashrc-fragment >> ~/.bashrc
     else
         printf "%s\n" "Activation script already exists in .bashrc"
     fi
+}
 
-    if [ -d ~/.config/i3 ]; then
-        printf "%s\n" "i3 config directory already exists"
-    else
-        ln -s "${ROOT}/config/i3" ~/.config/i3
-    fi
+function link-folder-if-not-exists {
+    source="$1"
+    target="$2"
 
-    if [ -d ~/.config/i3status ]; then
-        printf "%s\n" "i3status config directory already exists"
+    if [ ! -d "$source" ]; then
+        ln -s "$source" "$target"
     else
-        ln -s "${ROOT}/config/i3status" ~/.config/i3status
+        printf "%s\n" "Link: skipping ${source}"
     fi
-    
-    if [ -d ~/.config/i3blocks ]; then
-        printf "%s\n" "i3blocks config directory already exists"
-    else
-        ln -s "${ROOT}/config/i3blocks" ~/.config/i3blocks
-    fi
+}
 
-    sudo pacman -Syu vim dmenu gnome-terminal i3blocks firefox pamac pavucontrol terminus-font noto-fonts-emoji ttf-dejavu polkit-gnome nitrogen openssh code xorg-xbacklight keepassxc zenity
+function pacman-install {
+    if command -v pacman &> /dev/null; then
+        sudo pacman -Syu --needed "$@"
+    else
+        printf "%s\n" "pacman not found. Skipping"
+    fi
+}
+
+function prevent-dropbox-self-update {
+    # https://wiki.archlinux.org/index.php/Dropbox#Prevent_automatic_updates
     rm -rf ~/.dropbox-dist
     install -dm0 ~/.dropbox-dist
 }
