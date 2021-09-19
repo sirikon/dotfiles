3c675d7b0dbf51c1335b77e3c7d8941cb6a093c6
Carlos Fdez. Llamas <hello@sirikon.me>
2021-09-19 02:20:22 +0200

    remove setup machine

--- setup-machine.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-apt-get install sudo git
-usermod -a -G sudo sirikon
-sudo -u sirikon bash -c 'cd && git clone https://github.com/sirikon/dotfiles.git .sirikon && cd .sirikon && ./install.sh'
