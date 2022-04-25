5c5cb6af37fdc7cb8178c293fdd36e7c00b71c22
Carlos Fernández Llamas <sirikon@Carloss-MacBook-Pro.local>
2022-04-25 13:43:01 +0200

    activate for mac

---
+++ activate.mac.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+function git_branch {
+	branch=$(git branch 2>/dev/null | grep '^\*' | colrm 1 2)
+	if [ "$branch" == "" ]; then
+		echo ""
+	else
+		echo "[${branch}] "
+	fi
+}
+
+function python_venv {
+	if [ "$VIRTUAL_ENV" == "" ]; then
+		echo ""
+	else
+		echo "[🐍$(basename ${VIRTUAL_ENV})] "
+	fi
+}
+
+function prompt-normal {
+	PS1="\[\033[38;5;208m\]\u\[$(tput sgr0)\]\[\033[38;5;15m\] \[\033[38;5;248m\]\w \$(git_branch)\$(python_venv)\[$(tput sgr0)\]\[\033[38;5;214m\]\\$\[$(tput sgr0)\] "
+	export PS1
+}
+
+function prompt-tiny {
+	PS1="\[\033[38;5;214m\]\\$\[$(tput sgr0)\] "
+	export PS1
+}
+
+prompt-normal
